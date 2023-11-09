00303b091adf8feddc18cbd3c2ef6ae77c2c55e1
Brian <bstanton773@gmail.com>
2023-11-09 12:11:21 -0600

    Initial setup with webpack, TS, and live-server

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+console.log('Hello this is Brian!');
+console.log("This is another logged line");
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+;
+console.log(addNums(10, 20));
+
+/******/ })()
+;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello this is Brian!');
+console.log("This is another logged line");
+
+function addNums(num1:number, num2:number):number{
+    return num1 + num2
+};
+
+console.log(addNums(10, 20));
